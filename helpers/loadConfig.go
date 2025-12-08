98ade6750cdb5f71f913b5270bf8e19c6623d481
Francisco Borges <franciscomsborges2002@gmail.com>
2025-12-08 19:59:04 +0000

    fix: changed to hold .env for dev and envs for prod

--- helpers/loadConfig.go
+++ helpers/loadConfig.go
@@ -2,6 +2,8 @@ package helpers
 
 import (
 	"errors"
+	"log"
+	"os"
 	"steamshark-api/utils"
 
 	"github.com/joho/godotenv"
@@ -15,13 +17,18 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	err := godotenv.Load(".env")
-	if err != nil {
-		return nil, errors.New("error loading .env")
+	// Try to load .env only if it exists (so Coolify won't complain)
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(".ENV"); err != nil {
+			return nil, errors.New("error loading .env")
+		}
+	} else {
+		// No .env file – normal in Docker/Coolify
+		log.Println("No .env file found; using environment variables only.")
 	}
 
 	config := Config{
-		Env:  utils.GetEnv("ENV", "development"),
+		Env:  utils.GetEnv("APP_ENV", "development"),
 		Host: utils.GetEnv("HOST", "0.0.0.0"),
 		Port: utils.GetEnv("PORT", "8800"),
 		// default to databases/steamshark.db relative to the app WORKDIR
